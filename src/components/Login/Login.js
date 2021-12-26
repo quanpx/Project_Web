5c4cd1aa60f66f23a66655c3ad74a969254922f3
quanpx <quanphungxuan00@gmail.com>
2021-12-27 00:16:16 +0700

    add login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,76 +1,44 @@
-import React, {useState} from "react";
-import {useNavigate} from "react-router-dom";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Login.css";
+const axios = require('axios')
 
 export default function Login() {
     let history = useNavigate();
-    const [username, setUserName] = useState("");
-    const [password, setPassword] = useState("");
 
-    const setParams = (event) => {
-        if(event.target.name === 'username'){
-            setUserName(event.target.value);
-        }
-        console.log(username);
-        if(event.target.name === 'password'){
-            setPassword(event.target.value);
-        }
-        console.log(password);
-    }
-
+    let [logins,setLogins]=useState([]);
     const login = () => {
+
+
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+
+
         var myHeaders = new Headers();
         myHeaders.append("Content-type", "application/json");
 
-        var urlencoded = new URLSearchParams();
-        urlencoded.append("username", username);
-        urlencoded.append("password", password);
-
-        var requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: urlencoded,
-            redirect: "follow"
+       
+        const headers = {
+           'Authorization': 'Bearer '+logins[0].token,
+            'Content-Type':'application/json'
         };
+        let body = { username, password };
 
-        fetch("", requestOptions)
-            .then(response => {
-                console.log(response);
-                if(response.ok){
-                    return response.json();
-                }
-                throw new Error(response.status)
-            })
-            .then(result => {
-                console.log(result);
-                localStorage.setItem("accessToken", result.accessToken)
-                history.push("/home")
-            })
-            .catch(error => {
-                console.log('error', error);
-                alert("Username or Password is wrong!");
-            })
+        axios.post("http://localhost:8080/api/v1/login", body)
+            .then(res => setLogins(res.data.data));
+        
     }
 
-    return(
+    return (
         <div className="login">
             <form>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td><label className="username">Username:</label></td>
-                            <td><input type="text" name="username" onChange={setParams} /></td>
-                        </tr>
-                        <tr>
-                            <td><label className="password">Password:</label></td>
-                            <td><input type="text" name="password" onChange={setParams} /></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div>
-                    <button type="button" className="btn btn-primary" onClick={login}>Login</button>
-                </div>
-            </form>
-        </div>
+                <label className="username">Username:</label>
+                <input type="text" id="username" name="username" />
+
+                <label className="password">Password:</label>
+                <input type="text" id="password" name="password" />
+                <button type="button" className="btn btn-primary" onClick={login}>Login</button>
+            </form >
+        </div >
     )
 }
