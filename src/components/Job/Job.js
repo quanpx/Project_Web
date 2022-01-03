5ccd6ab2e0d3d9a929cf5dac2589ff4727f7e9ab
quanpx <quanphungxuan00@gmail.com>
2022-01-04 03:11:48 +0700

    add handle receive job itself

--- src/components/Job/Job.js
+++ src/components/Job/Job.js
@@ -3,12 +3,13 @@ import { Container, Card, Modal, Button } from 'react-bootstrap';
 import { Form, Input, InputNumber } from 'antd';
 import { GiPositionMarker } from "react-icons/gi";
 import { AiOutlineBulb } from "react-icons/ai";
-import { MdAttachMoney,  MdDateRange } from "react-icons/md";
+import { MdAttachMoney, MdDateRange } from "react-icons/md";
 import "./Job.css";
 import PageContent from "../PageContent/PageContent";
 import convertToVNese from "../../utils/convertToVNese";
 import NumberFormat from "react-number-format";
 import axios from "axios";
+import { notification } from "antd";
 
 
 function Job() {
@@ -33,8 +34,21 @@ function Job() {
         setShow(false);
         setActiveModal(null);
     }
+    const openNotificationWarning = (message) => {
+        notification.warning({
+            message: message,
+            duration: 3
+        });
+    }
 
+    const openNotificationSuccess = (message) => {
+        notification.success({
+            message: message,
+            duration: 2
+        });
+    }
     const handleGetjob = async (id) => {
+
         handleClose();
         let headers = {
             'Authorization': 'Bearer ' + authenticated.token,
@@ -51,108 +65,119 @@ function Job() {
             body,
             { headers }
         )
-            .then(res => console.log(res));
-    }
+            .then(res => {
+                if (res.data.code == 200) {
+                    openNotificationSuccess("Đã gửi yêu cầu nhận việc !");
+                }
+            }).catch(err => openNotificationWarning(err.message));
+            
+    
+}
+console.log(Items);
+const [activeModal, setActiveModal] = useState(null);
+const clickHandler = (e, index, username) => {
+    if (username === authenticated.user.username) {
+        openNotificationWarning("Bạn không thể nhận công việc mình tạo!");
 
-    const [activeModal, setActiveModal] = useState(null);
-    const clickHandler = (e, index) => {
+    } else {
         setActiveModal(index);
     }
+}
 
-    const layout = {
-        labelCol: { span: 8 },
-        wrapperCol: { span: 16 },
-    };
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
 
-    // Search
-    const { Search } = Input;
-    const [filterData, setFilterData] = useState([]);
+// Search
+const { Search } = Input;
+const [filterData, setFilterData] = useState([]);
 
-    const searchJob = (event) => {
-        setFilterData(event.target.value);
-    }
-    let dataSearch = Items.filter(item => {
-        return Object.keys(item).some(key => 
-            item[key].toString().toLowerCase().includes(filterData.toString().toLowerCase())
-        )
-    })
-
-    // job content
-    const jobContent = {
-        img: "https://images.unsplash.com/photo-1601737786679-f6883570cfb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-        line1: "Job",
-        line2: "Job nè",
-        line3: "Job nữa nè",
-        line4: "Job tiếp nè"
-    }
-
-    return (
-        <>
-            <PageContent content={jobContent} />
-            <Container>
-                <div className="search col-4 mx-auto mb-2">
-                    <Search
-                        type="text"
-                        placeholder="Search for..." 
-                        // enterButton 
-                        value={filterData}
-                        onChange={searchJob}
-                    />
-                </div>
-
-                <div className="jobs-content">
-                    <div className="list-jobs row justify-content-center align-items-center">
-                        {
-                            dataSearch.map((element, index) => {
-                                return (
-                                    <div key={index} className="col-sm-6 col-md-6 col-lg-4" key={index} data-aos="zoom-in-up">
-                                        <Card className="card-active">
-                                            <Card.Img className="mx-auto" src={element.image_url} />
-                                            <Card.Body>
-                                                <Card.Title>{element.name}</Card.Title>
-                                                <Card.Text>
-                                                    {element.address}<br />
-                                                    <NumberFormat value={element.salary} displayType={'text'} thousandSeparator={true} suffix={' VND'}/><br />
-                                                    {element.due}<br />
-                                                    {convertToVNese(element.status)}
-                                                </Card.Text>
-                                                <Button className="detail-btn" href={`./job-detail/${element.id}`}>Chi tiết</Button>
-                                                {
-                                                    element.status == "PENDING" || element.status=="COMPLETED" ? <Button className="getJob-btn" disabled>Nhận việc</Button> :
-                                                        <Button className="getJob-btn" onClick={(e) => clickHandler(e, index)}>Nhận việc</Button>
-                                                }
-
-                                                <Modal show={activeModal === index} onHide={handleClose} centered>
-                                                    <Modal.Header closeButton>
-                                                        <Modal.Title>Nhận việc</Modal.Title>
-                                                    </Modal.Header>
-                                                    <Modal.Body>
-                                                        <Form {...layout}>
-                                                            <Form.Item name={['user', 'salary']} label="Lương mong muốn" rules={[{ type: 'number', min: 100000, max: 10000000 }]}>
-                                                                <InputNumber step={100000} id={`user_salary${element.id}`} />
-                                                            </Form.Item>
-                                                            <Form.Item name={['user', 'note']} label="Yêu cầu">
-                                                                <Input.TextArea id={`user_note${element.id}`} />
-                                                            </Form.Item>
-                                                        </Form>
-                                                    </Modal.Body>
-                                                    <Modal.Footer>
-                                                        <Button variant="secondary" onClick={() => handleGetjob(element.id)}>
-                                                            Nhận việc
-                                                        </Button>
-                                                    </Modal.Footer>
-                                                </Modal>
-                                            </Card.Body>
-                                        </Card>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            </Container>
-        </>
+const searchJob = (event) => {
+    setFilterData(event.target.value);
+}
+let dataSearch = Items.filter(item => {
+    return Object.keys(item).some(key =>
+        item[key].toString().toLowerCase().includes(filterData.toString().toLowerCase())
     )
+})
+
+// job content
+const jobContent = {
+    img: "https://images.unsplash.com/photo-1601737786679-f6883570cfb5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+    line1: "Job",
+    line2: "Job nè",
+    line3: "Job nữa nè",
+    line4: "Job tiếp nè"
+}
+
+return (
+    <>
+        <PageContent content={jobContent} />
+        <Container>
+            <div className="search col-4 mx-auto mb-2">
+                <Search
+                    type="text"
+                    placeholder="Search for..."
+                    // enterButton 
+                    value={filterData}
+                    onChange={searchJob}
+                />
+            </div>
+
+            <div className="jobs-content">
+                <div className="list-jobs row justify-content-center align-items-center">
+                    {
+                        dataSearch.map((element, index) => {
+                            return (
+                                <div key={index} className="col-sm-6 col-md-6 col-lg-4" key={index} data-aos="zoom-in-up">
+                                    <Card className="card-active">
+                                        <Card.Img className="mx-auto" src={element.image_url} />
+                                        <Card.Body>
+                                            <Card.Title>{element.name}</Card.Title>
+                                            <Card.Text>
+                                                {element.address}<br />
+                                                <NumberFormat value={element.salary} displayType={'text'} thousandSeparator={true} suffix={' VND'} /><br />
+                                                {element.due}<br />
+                                                {convertToVNese(element.status)}
+                                            </Card.Text>
+                                            <Button className="detail-btn" href={`./job-detail/${element.id}`}>Chi tiết</Button>
+                                            {
+                                                element.status == "PENDING" || element.status == "COMPLETED" ? <Button className="getJob-btn" disabled>Nhận việc</Button> :
+                                                    <Button className="getJob-btn" onClick={(e) => clickHandler(e, index, element.username)}>Nhận việc</Button>
+                                            }
+
+                                            <Modal show={activeModal === index} onHide={handleClose} centered>
+                                                <Modal.Header closeButton>
+                                                    <Modal.Title>Nhận việc</Modal.Title>
+                                                </Modal.Header>
+                                                <Modal.Body>
+                                                    <Form {...layout}>
+                                                        <Form.Item name={['user', 'salary']} label="Lương mong muốn" rules={[{ type: 'number', min: 100000, max: 10000000 }]}>
+                                                            <InputNumber step={100000} id={`user_salary${element.id}`} />
+                                                        </Form.Item>
+                                                        <Form.Item name={['user', 'note']} label="Yêu cầu">
+                                                            <Input.TextArea id={`user_note${element.id}`} />
+                                                        </Form.Item>
+                                                    </Form>
+                                                </Modal.Body>
+                                                <Modal.Footer>
+                                                    <Button variant="secondary" onClick={() => handleGetjob(element.id, element.username)}>
+                                                        Nhận việc
+                                                    </Button>
+                                                </Modal.Footer>
+                                            </Modal>
+                                        </Card.Body>
+                                    </Card>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        </Container>
+    </>
+)
 };
 
 export default React.memo(Job);
